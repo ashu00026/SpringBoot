2e4dc4d571010b1c9da08148339ec22520859806
ashu00026 <19341a1207@gmrit.edu.in>
2023-11-03 00:46:41 +0530

    section 10 completed

--- spring security/Security/section10/src/main/java/com/company/Respository/LoanRepository.java
+++ spring security/Security/section10/src/main/java/com/company/Respository/LoanRepository.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 import com.company.Model.Loans;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
-
+    @PreAuthorize("hasRole('USER')")
     List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
